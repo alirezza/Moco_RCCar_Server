972d80818837ad9b9a710ad695878ac89c73809f
alireza-conti <alireza.naderi@continental-corporation.com>
2022-02-22 08:29:00 +0100

    GUI updated

--- Server/Server.py
+++ Server/Server.py
@@ -1,10 +1,26 @@
 import os
 import sys
-
-from PySide6.QtWidgets import QApplication, QPushButton, QDialog, QVBoxLayout, QLabel, QFileDialog, QSlider
+from PySide6.QtWidgets import (
+    QWidget,
+    QVBoxLayout,
+    QLabel,
+    QCheckBox,
+    QApplication,
+    QDialog,
+    QTabWidget,
+    QLineEdit,
+    QDialogButtonBox,
+    QFrame,
+    QListWidget,
+    QGroupBox, QPushButton,
+)
+from PySide6.QtWidgets import QApplication, QPushButton, QDialog, QVBoxLayout, QLabel, QFileDialog, QSlider, QWidget, \
+    QGroupBox, QTabWidget, QDialogButtonBox
 from PySide6.QtCore import *
 import threading
 import time
+
+from PySide6.examples.widgets.dialogs.tabdialog.tabdialog import TabDialog
 
 from State_ControlCar import StateControlCar
 from State_PathDetect import StatePathDetect
@@ -117,52 +133,120 @@ def changeDir():
         print("Direction changed")
 
 
-class Form(QDialog):
+class TabDialog(QDialog):
+    def __init__(self, parent: QWidget = None):
+        super().__init__(parent)
 
-    def __init__(self, parent=None):
-        super(Form, self).__init__(parent)
+        tab_widget = QTabWidget()
+        tab_widget.addTab(MainTab(self), "Main")
+        tab_widget.addTab(PathTab(self), "Path")
+        tab_widget.addTab(SettingTab(self), "Applications")
 
-        # Create widgets
-        self.button1 = QPushButton("Reset State Machine")
+        button_box = QDialogButtonBox(
+            QDialogButtonBox.Cancel
+        )
+
+        button_box.rejected.connect(self.reject)
+
+        main_layout = QVBoxLayout()
+        main_layout.addWidget(tab_widget)
+        main_layout.addWidget(button_box)
+        self.setLayout(main_layout)
+        self.setWindowTitle("RC Car")
+        self.resize(300, 500)
+
+
+
+
+class MainTab(QWidget):
+    def __init__(self, parent: QWidget):
+        super().__init__(parent)
+        main_group = QGroupBox("Main")
+        self.button6 = QPushButton("Start Car")
+        self.button6.resize(250,50)
+        self.button7 = QPushButton("Stop Car")
+        self.button8 = QPushButton("Park")
+        self.button9 = QPushButton("Continue")
+        information_gruop = QGroupBox("Information")
+        self.statusText_velo = QLabel("Speed: ")
+        self.statusText_angle = QLabel("Steering angle: ")
+
+        main_layout = QVBoxLayout()
+        main_layout.addWidget(self.button6)
+        main_layout.addWidget(self.button7)
+        main_layout.addWidget(self.button8)
+        main_layout.addWidget(self.button9)
+        main_group.setLayout(main_layout)
+
+        information_layout = QVBoxLayout()
+        information_layout.addWidget(self.statusText_velo)
+        information_layout.addWidget(self.statusText_angle)
+        information_gruop.setLayout(information_layout)
+
+        main_layout = QVBoxLayout()
+        main_layout.addWidget(main_group)
+        main_layout.addWidget(information_gruop)
+        main_layout.addStretch(1)
+        self.setLayout(main_layout)
+
+        self.button6.clicked.connect(start_car)
+        self.button7.clicked.connect(stop_car)
+        self.button8.clicked.connect(park_car)
+        self.button9.clicked.connect(continue_car)
+
+
+class PathTab(QWidget):
+    def __init__(self, parent: QWidget):
+        super().__init__(parent)
+
+        path_group = QGroupBox("Path")
+
         self.button2 = QPushButton("Lock In Path")
         self.button3 = QPushButton("Clear Path")
         self.button4 = QPushButton("Load Path")
         self.button5 = QPushButton("Save Path")
-        self.button6 = QPushButton("Start Car")
-        self.button7 = QPushButton("Stop Car")
-        self.button8 = QPushButton("Park")
-        self.button9 = QPushButton("Continue")
-        self.button10 = QPushButton("Reset Corners")
         self.button11 = QPushButton("Create new Path")
+
+        path_layout = QVBoxLayout()
+        path_layout.addWidget(self.button2)
+        path_layout.addWidget(self.button3)
+        path_layout.addWidget(self.button5)
+        path_layout.addWidget(self.button4)
+        path_group.setLayout(path_layout)
+
+        main_layout = QVBoxLayout()
+        main_layout.addWidget(path_group)
+        main_layout.addStretch(1)
+        self.setLayout(main_layout)
+        self.button2.clicked.connect(create_path)
+        self.button3.clicked.connect(clear_path)
+        self.button4.clicked.connect(lambda: load_path(self))
+        self.button5.clicked.connect(lambda: save_path(self))
+        self.button11.clicked.connect(newPath)
+
+class SettingTab(QWidget):
+    def __init__(self, parent: QWidget):
+        super().__init__(parent)
+
+        setting_group = QGroupBox("Setting")
+
+        self.button1 = QPushButton("Reset State Machine")
         self.button12 = QPushButton("Change Direction")
-        #self.slider1 = QSlider(Qt.Horizontal)
-        #self.statusText = QLabel("Status Label")
-        #self.statusText1 = QLabel("Status Label")
+        self.button10 = QPushButton("Reset Corners")
 
-        #self.slider1.setRange(0, 150)
-        #self.slider1.setTickInterval(1)
-        #self.slider1.setValue(ServerConfig.getInstance().vehicle_const_speed)
+        setting_layout = QVBoxLayout()
+        setting_layout.addWidget(self.button1)
+        setting_layout.addWidget(self.button12)
+        setting_layout.addWidget(self.button10)
+        setting_group.setLayout(setting_layout)
 
-        # Create layout and add widgets
-        layout = QVBoxLayout()
-        layout.addWidget(self.button1)
-        layout.addWidget(self.button6)
-        layout.addWidget(self.button7)
-        layout.addWidget(self.button8)
-        layout.addWidget(self.button9)
-        layout.addWidget(self.button2)
-        layout.addWidget(self.button3)
-        layout.addWidget(self.button5)
-        layout.addWidget(self.button4)
-        layout.addWidget(self.button10)
-        layout.addWidget(self.button11)
-        layout.addWidget(self.button12)
-        #layout.addWidget(self.statusText)
-        #layout.addWidget(self.slider1)
-        #layout.addWidget(self.statusText1)
-        # Set dialog layout
-        self.setLayout(layout)
-
+        main_layout = QVBoxLayout()
+        main_layout.addWidget(setting_group)
+        main_layout.addStretch(1)
+        self.setLayout(main_layout)
+        self.button10.clicked.connect(resetCorner)
+        self.button12.clicked.connect(changeDir)
+        self.button1.clicked.connect(restart_state_machine)
 
 # Create the Qt Application
 app = QApplication([])
@@ -171,19 +255,8 @@ app = QApplication([])
 myLogicThread = LogicLoop()
 LogicThread = threading.Thread(target=myLogicThread.loop, args=())
 
-myDialog = Form()
-myDialog.button1.clicked.connect(restart_state_machine)
-myDialog.button2.clicked.connect(create_path)
-myDialog.button3.clicked.connect(clear_path)
-myDialog.button4.clicked.connect(lambda: load_path(myDialog))
-myDialog.button5.clicked.connect(lambda: save_path(myDialog))
-myDialog.button6.clicked.connect(start_car)
-myDialog.button7.clicked.connect(stop_car)
-myDialog.button8.clicked.connect(park_car)
-myDialog.button9.clicked.connect(continue_car)
-myDialog.button10.clicked.connect(resetCorner)
-myDialog.button11.clicked.connect(newPath)
-myDialog.button12.clicked.connect(changeDir)
+myDialog = TabDialog()
+
 #myDialog.slider1.valueChanged.connect(lambda: set_velocity(myDialog.slider1.value()))
 myDialog.show()
 
